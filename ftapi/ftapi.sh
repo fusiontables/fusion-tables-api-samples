412ee44d89d5acf08395e43b8765f7c37bae3353
arl@google.com <arl@google.com@e3d4e609-9666-49d2-321f-d09c54e1dbad>
2012-10-15 18:36:29 +0000

    Explain curl command lines in script comments.

--- ftapi/ftapi.sh
+++ ftapi/ftapi.sh
@@ -3,9 +3,18 @@
 
 uri=${1:-tables}
 shift
+
+# Obtain a fresh OAuth access token, if necessary
 source credentials.sh
 source common.sh
 ensure_fresh_access_token
+
+# Invoke Fusion Tables API via curl. Option, -s for less output, but -S to
+# print output on failure. Other arguments from the command line, for example
+# -d ... or -T ... Authorization header per OAuth 2. Content type header, which
+# is required when there is a message body (PUT and some POST), and harmless
+# otherwise. Finally the URL with the suffix from the command line's first
+# argument.
 curl -s -S $* -H "Authorization: Bearer $access_token" \
   -H "Content-Type: application/json" \
   "https://www.googleapis.com/fusiontables/v1/$uri"
--- ftapi/ftsql.sh
+++ ftapi/ftsql.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 # Invokes the Fusion Tables API with given SQL
 
+# Obtain a fresh OAuth access token, if necessary
 source credentials.sh
 source common.sh
 ensure_fresh_access_token
-curl -d sql="$*" -H "Authorization: Bearer $access_token" "https://www.googleapis.com/fusiontables/v1/query?alt=csv"
+
+# Invoke Fusion Tables query via curl. Option, -s for less output, but -S to
+# print output on failure. Other arguments from the command are joined for the
+# sql parameter. Authorization header per OAuth 2. Finally the URL of the query
+# API with option alt=csv, because the default (JSON) is harder to read for
+# humans.
+curl -s -S -d sql="$*" -H "Authorization: Bearer $access_token" \
+  "https://www.googleapis.com/fusiontables/v1/query?alt=csv"
